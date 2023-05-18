378e79eed789d75ba0d32321b652ee5d2a8b28a0
Sumedha012 <sumedham0112@gmail.com>
2023-05-18 17:17:28 +0530

    created votrid VoterIdInfo and SigninService

--- backend/src/main/java/com/example/restservice/repositories/VoterIdInfoRepository.java
+++ backend/src/main/java/com/example/restservice/repositories/VoterIdInfoRepository.java
@@ -1,5 +1,11 @@
 package com.example.restservice.repositories;
 
-public class VoterIdInfoRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.restservice.entities.VoterIdInfo;
+
+public interface VoterIdInfoRepository extends CrudRepository<VoterIdInfo, String>{
+
+    void save(String string, String string2, String string3, int i);
     
 }
